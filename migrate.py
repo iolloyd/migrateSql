b6abba32745500569b23bd55cf3f65a7ac613f28
lloyd <manchesterboy@gmail.com>
2013-10-23 11:26:44 +0200

    add ability to add default values with new columns in inserts

--- migrate.py
+++ migrate.py
@@ -1,37 +1,39 @@
 from pprint import pprint
 from string import split, strip
 
+
 def getMappings(x):
     mappings = {
-            'orderItems': {
-                'add': {
-                    'baseValue':
-
+        'billerActiveCurrencies': {
+            'columns': {
+                'default': 'is_default'
+            }
+        },
+        'clients': {
+            'columns': {
+                'APIKey': 'apiKey'
+            },
+        },
+        'languages': {
+            'columns': {
+                'languageID': 'id'
+            },
+            'add': {
+                'title': 'xxx'
+            },
+        },
+        'orderItems': {
+            'add': {
+                'baseValue': ''
+            }
+        },
         'orderAddresses': {
             'name': 'order_addresses',
             'columns': {
                 'orderAddressID': 'order_address_id',
                 'orderID': 'order_id',
                 'customerAddressID': 'customer_address_id',
-                }
-            },
-        'billerActiveCurrencies': {
-            'columns': {
-                'default': 'is_default'
-                }
-            },
-        'clients': {
-            'columns': {
-                'APIKey': 'apiKey'
-                },
-            },
-        'languages': {
-            'columns': {
-                'languageID': 'id'
-            },
-            'add': {
-                'title': ''
-            },
+            }
         },
         'storeCoupons': {
             'add': {
@@ -44,16 +46,30 @@ def getMappings(x):
         return mappings[x]
     return None
 
-def justUseful(x):
-    lines = split(x, '\n')
-    return lines
-    return ''.join(lines)
 
-label        = lambda x, mapping: mapping.get(x, x)
-isInsert     = lambda x: len(x.split('INSERT INTO')) > 1
-hasInserts   = lambda x: len(x['inserts']) > 0
+justUseful = lambda x: split(x, '\n')
+label = lambda tableName, mapping: mapping.get(tableName, tableName)
+isInsert = lambda x: len(x.split('INSERT INTO')) > 1
+hasInserts = lambda t: len(t['inserts']) > 0
 blacklist = ['alerts', 'emailQueue']
-ignoreBlacklist = lambda x: x['name'] not in blacklist
+ignoreBlacklist = lambda t: t['name'] not in blacklist
+
+def addMissingColumns(table, mapping):
+    m = mapping['add']
+    table['inserts'] = map(lambda x, m=m: injectValue(x, m), table['inserts'])
+    return table
+
+def injectValue(insert, mapping):
+    chunks= split(insert, '(', 2)
+    for newColumn, newValue in mapping.items():
+        chunks[1] = "`%s`, %s" % (newColumn, chunks[1])
+        if type(1) == type(newValue):
+            chunks[2] = "%s, %s" % (newValue, chunks[2])
+        else:
+            chunks[2] = "'%s', %s" % (newValue, chunks[2])
+    chunks[1] = "(%s" % chunks[1]
+    chunks[2] = "(%s" % chunks[2]
+    return ''.join(chunks)
 
 def parseTable(x):
     tableName, rest = split(x, '(\n')
@@ -61,13 +77,14 @@ def parseTable(x):
     rest = split(rest, ');\n')
     body = split(rest[0], '/*')
     body = split(body[0], ';\n')[0]
-    inserts = map(lambda x: "INSERT INTO %s" % x, split(rest[0], "INSERT INTO")[1:])
+    inserts = map(lambda line: "INSERT INTO %s" % line, split(rest[0], "INSERT INTO")[1:])
     return {
         'body': body,
         'name': strip(tableName, ' '),
         'inserts': inserts,
         'rest': rest
-        }
+    }
+
 
 def migrateTable(x):
     mapping = getMappings(x['name'])
@@ -81,19 +98,22 @@ def migrateTable(x):
     for k, v in mapping['columns'].items():
         x['body'] = x['body'].replace(k, v)
         x['inserts'] = map(lambda i: i.replace(k, v), x['inserts'])
+
+    if mapping.get('add', False):
+        x = addMissingColumns(x, mapping)
     return x
 
+
 def showTable(x):
-    """
     if len(x['inserts']) > 0:
         print "DROP TABLE if exists %s;" % x['name']
         print "CREATE TABLE %s (\n%s;\n" % (x['name'], x['body'])
 
-    """
-    for x in x['inserts']:
-        print "%s);" % x
-"""Enter sql dump filename and new database name
-"""
+def showInserts(x):
+    for table in x['inserts']:
+        print "%s);" % table
+
+
 filename = 'fullDump.sql'
 database = 'tf_framework'
 
@@ -113,5 +133,7 @@ tables = map(migrateTable, tables) # map correct table and column names
 for x in tables:
     showTable(x)
 
-print 'SET FOREIGN_KEY_CHECKS = 1;'
+for x in tables:
+    showInserts(x)
 
+print 'SET FOREIGN_KEY_CHECKS = 1;'
