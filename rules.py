20401876d4764bad1fe272fe06a681467eed9dd8
lloyd <manchesterboy@gmail.com>
2013-10-24 16:22:49 +0200

    add more mappings

--- rules.py
+++ rules.py
@@ -1,77 +1,184 @@
 mappings = {
-        'acl': {
-            'name': 'acl'
-            },
-        'billerActiveCurrencies': {
-            'columns': {
-                'default': 'is_default'
-                }
-            },
-        'clients': {
-            'columns': {
-                'APIKey': 'apiKey'
-                },
-            },
-        'languages': {
-            'columns': {
-                'languageID': 'id'
-                },
-            'add': {
-                'title': ''
-                },
-            },
-        'orders': {
-            'populate': [
-                {
-                    'rule': 'orders.orderID = billerTransactionRequests.orderID',
-                    'columns': {
-                        'original': ['shippingAddressID', ],
-                        'new': ['shippingAddressID', ],
-                        }
-                    }
-                ]
-            },
-        'orderItems': {
-            'add': {
-                'baseValue': ''
-                }
-            },
-        'orderAddresses': {
-            'name': 'order_addresses',
-            'columns': {
-                'orderAddressID': 'order_address_id',
-                'orderID': 'order_id',
-                'customerAddressID': 'customer_address_id',
-                },
-            'populate': [
-                {
-                    'rule': 'orderAddresses.customerAddressID = customerAddress.customerAddressID',
-                    'columns': {
-                        'original': ['firstName', 'lastName', 'address1', 'address2', 'city', 'state', 'country', 'zip', 'phone', 'status', 'created', 'modified'],
-                        'new': ['firstName', 'lastName', 'address1', 'address2', 'city', 'state', 'country', 'zip', 'phone', 'status', 'created', 'modified']
-                        }
-                    },
-                {
-                    'rule': 'orderAddresses.orderAddressID = billerTransactionRequests.id',
-                    'columns': {
-                        'original': ['billingAddressID'],
-                        'new': ['billingAddressID']
-                        }
-                    }
-                ]
-            },
-        'orderQuotes': {
-                'name': 'orderInvoices'
-                },
-        'storeCoupons': {
-                'add': {
-                    'autoEnable': 0,
-                    'approval_code': ''
-                    },
-                },
-        'tagDomains': {
-                'columns': {
-                    'targetURL': 'defaultURL'
-                    }
-                }
+    'billerActiveCurrencies': {
+        'columns': {
+            'default': 'is_default'
         }
+    },
+
+    'billerCardTypes': {
+        'add': {
+            'key': ''
+        }
+    },
+
+    'billerPaymentDetails': {
+        'populate': [
+            {
+                'rule': 'WHERE ',
+                'columns': {
+                    'original' : [
+                        'billerPaymentDetailID' ,
+                        'customerID' ,
+                        'cardNumber' ,
+                        'cardExpiryMonth',
+                        'cardExpiryYear' ,
+                        'billerCardTypeID' ,
+                        'storedbillerID' ,
+                        'active' ,
+                    ],
+                    'new' : [
+                        'billerPaymentDetailID' ,
+                        'customerID' ,
+                        'cardNumber' ,
+                        'cardExpiryMonth',
+                        'cardExpiryYear' ,
+                        'billerCardTypeID' ,
+                        'billerID' ,
+                        'active' ,
+                    ],
+                }
+            }
+        ]
+    },
+
+    'clients': {
+        'columns': {
+            'APIKey': 'apiKey'
+        },
+    },
+    'customerAddresses': {
+    },
+    'languages': {
+        'columns': {
+            'languageID': 'id'
+            },
+        'add': {
+            'title': ''
+            },
+        },
+    'orders': {
+        'populate': [
+            {
+                'rule': 'WHERE orders.orderID = billerTransactionRequests.orderID',
+                'columns': {
+                    'original': ['shippingAddressID', ],
+                    'new': ['shippingAddressID', ],
+                    }
+                }
+            ]
+        },
+    'orderItems': {
+        'add': {
+            'baseValue': ''
+            }
+        },
+    'orderAddresses': {
+        'name': 'order_addresses',
+        'columns': {
+            'orderAddressID': 'order_address_id',
+            'orderID': 'order_id',
+            'customerAddressID': 'customer_address_id',
+            },
+        'populate': [
+            {
+                'rule': 'WHERE orderAddresses.customerAddressID = customerAddress.customerAddressID',
+                'columns': {
+                    'original': [],
+                    'new': [],
+                    }
+                },
+            {
+                'rule': 'WHERE orderAddresses.orderAddressID = billerTransactionRequests.id',
+                'columns': {
+                    'original': ['billingAddressID'],
+                    'new': ['billingAddressID']
+                }
+            }
+        ]
+    },
+    'orderQuotes': {
+        'name': 'orderInvoices'
+    },
+    'orderShipments': {
+            'populate': {
+                'rule': 'WHERE orderShipments.orderID=order.orderID',
+                'columns': {
+                    'original': [],
+                    'new': []
+                }
+            }
+    },
+    'productRanges': {
+        'add': {
+            'name': ''
+        }
+    },
+    'storeCoupons': {
+        'add': {
+            'approval_code': ''
+        },
+    },
+    'storeCouponUses': {
+        'columns': {
+            'transactionRequestID': 'orderID'
+        },
+    },
+    'tagDomains': {
+        'columns': {
+            'targetURL': 'defaultURL'
+        }
+    }
+}
+
+legacyOnly = [
+    'acl',
+    'aclGroups',
+    'aclPermissions',
+    'aclRoles', 'customFieldBindings',
+    'emailQueue',
+    'invoices',
+    'ipranges',
+    'productReviews',
+    'promotionRulesMeta',
+
+]
+
+insertOnly = [
+    'agencies',
+    'billerCards',
+    'billerCosts',
+    'billerCurrencies',
+    'billerCurrencyRates',
+    'billers',
+    'billerStoredDetails',
+    'billerTransactionRequests',
+    'billerTransactions',
+    'customerAuthentication',
+    'customerBlacklists',
+    'customers',
+    'customFieldBindings',
+    'customFields',
+    'handsetProviderDevices',
+    'handsetProviders',
+    'orderAddresses',
+    'orderItems',
+    'productCategories',
+    'productCategoryValues',
+    'productDefaultFields',
+    'productFields',
+    'productFieldValues',
+    'productImageGroups',
+    'productImages',
+    'products',
+    'promotionRewardPriceDiscounts',
+    'promotionRewards',
+    'promotionRules',
+    'promotions',
+    'storeCategories',
+    'storeCategoryValues'
+    'storeCountryBlacklists',
+    'storeErrorStrings',
+    'storeImageGroups',
+    'storeImages',
+
+]
